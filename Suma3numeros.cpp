220be551d37e63c6aa4ddc87eff7f961602a7f16
Harold Cruz <sauriohydc@gmail.com>
2025-02-07 23:32:48 -0400

    Simplificado el código para sumar tres números

--- Suma3numeros.cpp
+++ Suma3numeros.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
 int main() {
-    double num1, num2, num3, suma;
+    double num1, num2, num3;
 
-    std::cout << "Ingrese el primer número: ";
-    std::cin >> num1;
+    std::cout << "Ingrese tres números: ";
+    std::cin >> num1 >> num2 >> num3;
 
-    std::cout << "Ingrese el segundo número: ";
-    std::cin >> num2;
-
-    std::cout << "Ingrese el tercer número: ";
-    std::cin >> num3;
-
-    suma = num1 + num2 + num3;
+    double suma = num1 + num2 + num3;
 
     std::cout << "La suma de los tres números es: " << suma << std::endl;
 
